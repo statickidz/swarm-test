fca53e6398b3acc67f0ecce88a0187d0dab6ac2d
Adrián Barrio Andrés <statickidz@gmail.com>
2024-10-14 11:59:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const ip = require('ip');
 const app = express();
 const port = 3000;
+
+const os = require( 'os' );
+const networkInterfaces = Object.values(os.networkInterfaces())
+    .reduce((r,a) => {
+        r = r.concat(a)
+        return r;
+    }, [])
+    .filter(({family, address}) => {
+        return family.toLowerCase().indexOf('v4') >= 0 &&
+            address !== '127.0.0.1'
+    })
+    .map(({address}) => address);
+const ipAddresses = networkInterfaces.join(', ')
 
 // Generar un identificador único para esta instancia
 const instanceId = Math.random().toString(36).substring(7);
@@ -15,14 +27,14 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
   // Definir las rutas después del delay
   app.get('/', (req, res) => {
-    res.send(`Hello from instance ${instanceId} with ip ${ip}`);
+    res.send(`Hello from instance ${instanceId} with ip ${ipAddresses}`);
   });
 
   app.get('/health', (req, res) => {
-     res.send(`Hello from instance ${instanceId} with ip ${ip}`);
+     res.send(`Hello from instance ${instanceId} with ip ${ipAddresses}`);
   });
 
   app.listen(port, () => {
-    console.log(`App instance ${instanceId} with ip ${ip} running on port ${port} after 5 seconds delay`);
+    console.log(`App instance ${instanceId} with ip ${ipAddresses} running on port ${port} after 5 seconds delay`);
   });
 })();
