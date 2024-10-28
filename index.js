c259fec0f0ecc77d844331abd77ef6d835e7e38c
Adrián Barrio <statickidz@gmail.com>
2024-10-28 08:10:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = 3000;
 
 const os = require( 'os' );
 const networkInterfaces = Object.values(os.networkInterfaces())
